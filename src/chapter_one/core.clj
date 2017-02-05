a73369d24b86d4d3d4c9136eaba00a8bbd48a620
Jacob Malter <jdmalter@ncsu.edu>
2017-02-05 16:53:49 -0500

    I'm learning Clojure!

---
+++ src/chapter_one/core.clj
@@ -0,0 +1,6 @@
+(ns chapter_one.core)
+
+; Takes the average of some numbers
+(defn average
+  [numbers]
+  (/ (apply + numbers) (count numbers)))
---
+++ test/chapter_one/core-test.clj
@@ -0,0 +1,8 @@
+(ns chapter_one.core-test
+  (:require [clojure.test :refer :all]
+            [chapter_one.core :refer :all]))
+
+; Check that the average of [60 80 100 400] is 160
+(deftest average-test
+  (testing "FIXME, I fail."
+    (is (= (average [60 80 100 400]) 160))))
