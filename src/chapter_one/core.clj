4f3eed7ef3b5f7f4d55e1462d72b9e97a461f34f
Jacob Malter <jdmalter@ncsu.edu>
2017-02-06 14:25:12 -0500

    Completed chapter 1.

--- src/chapter_one/core.clj
+++ src/chapter_one/core.clj
@@ -4,3 +4,59 @@
 (defn average
   [numbers]
   (/ (apply + numbers) (count numbers)))
+
+; Read a string and return a literal
+(read-string "42")
+
+; Read a string a return a string literal
+(pr-str [1 2 3])
+; Declare a string literal
+"hello world!"
+
+; Declare a multiline string literal
+"multiline stings
+are easy to declare"
+
+; Declare some character literal
+(class \c) ;= java.lang.Character
+\u00ff ;= \ÿ
+\o41 ;= \!
+
+; Declare a keyword which evaluates to itself
+(def person {:name "Jacob Malter"
+             :city "Raleigh, NC"})
+
+; Declare two keywords with the same name but in different namespaces
+(def calzones {:name "DP Dough"
+               :location "Raleigh, NC"
+               ::location "35.779385, -78.675592"}) ; every tuesday
+
+; Declare a regex
+(def regex #"(\d+)-(\d+)")
+(class regex)
+(re-seq regex "1-3")
+
+
+; Use the reader macro
+(read-string "(+ 1 2 #_(* 2 2) 8)")
+
+; Use comment
+(comment (println "hello"))
+
+; Demonstrate the commas evaluate to whitespace
+(= [1 2 3] [1, 2, 3])
+
+; Show off literal data structure
+'(a b :name 12.5) ;; list
+['a 'b :name 12.5] ;; vector
+{:name "Jacob" :age 20} ;; map
+#{1 2 3} ;; set
+
+; define a variable
+(def x 1)
+x ;= 1
+
+; Demonstrate what eval does
+(eval :foo) ;= :foo
+(eval [1 2 3]) ;= [1 2 3]
+(eval "text") ;= "text"
--- test/chapter_one/core-test.clj
+++ test/chapter_one/core-test.clj
@@ -6,3 +6,28 @@
 (deftest average-test
   (testing "FIXME, I fail."
     (is (= (average [60 80 100 400]) 160))))
+
+; Check that read-string returns a literal
+(deftest read-string-test
+  (testing "FIXME, I fail."
+    (is (= (read-string "42") 42))))
+
+; Check that pr-str returns a string
+(deftest pr-str-test
+  (testing "FIXME, I fail."
+    (is (= (pr-str [1 2 3]) "[1 2 3]"))))
+
+; Check that a keyword is properly declared
+(deftest keyword-test
+  (testing "FIXME, I fail."
+    (is (= (:name person) "Jacob Malter"))))
+
+; Check that there is naming collision
+(deftest namespace-test
+  (testing "FIXME, I fail."
+    (is (= (:chapter_one.core/location calzones) "35.779385, -78.675592"))))
+
+; Check that comma evalues to whitespace
+(deftest comma-test
+  (testing "FIXME, I fail."
+    (is (= [1 2 3] [,,,1, 2,,, ,,,, 3,,,,,,,,,,,,,,]))))
